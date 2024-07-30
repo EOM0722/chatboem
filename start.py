e9fd56f29f5840c23a3cf78c43d201096f1f86ca
EOM0722 <djawpgus@appleden.com>
2024-07-30 10:53:43 +0900

    Initial commit

---
+++ start.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/')
+def index():
+    return send_from_directory('html', "index.html")
+
+@app.route('/<path:name>') #
+def start(name):
+    return send_from_directory('html', name)
